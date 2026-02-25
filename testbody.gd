e1d73c57da5dd9029ffca2350850fc0dd02ed695
beepobb <yeejiazhen23@gmail.com>
2026-02-25 08:21:01 +0800

    feat: snapping done but animation origin different

--- testbody.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody3D
-
-@onready var xr_origin: Node3D = $XROrigin3D
-
-func _process(_delta):
-	# Keep the body aligned to the XR origin position (roomscale)
-	global_position.x = xr_origin.global_position.x
-	global_position.z = xr_origin.global_position.z
