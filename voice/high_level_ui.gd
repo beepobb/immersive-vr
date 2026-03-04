0ad38e299fbac2d4cdf84cc71678e2514133a53f
phoebekuek <phoebekuek@gmail.com>
2026-03-04 13:17:16 +0800

    added multiplayer feature to test the voice

---
+++ voice/high_level_ui.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_server_pressed() -> void:
+	HighLevelNetworkExample.start_server() # Replace with function body.
+
+
+func _on_client_pressed() -> void:
+	HighLevelNetworkExample.start_client() # Replace with function body.
