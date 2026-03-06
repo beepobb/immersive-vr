92908cecaeb9cc8fa4b7431ba9f75dc1c16285e6
phoebekuek <phoebekuek@gmail.com>
2026-03-06 10:55:33 +0800

    retry

--- voice/high_level_player_spawner.gd
+++ voice/high_level_player_spawner.gd
@@ -4,13 +4,12 @@ extends MultiplayerSpawner
 
 func _ready() -> void:
 	multiplayer.peer_connected.connect(spawn_player)
-	
 
-func spawn_player(id:int)-> void:
-	if!multiplayer.is_server(): return 
-	
-	var player: Node= network_player.instantiate()
-	player.name= str(id)
-	
+func spawn_player(id: int) -> void:
+	if not multiplayer.is_server():
+		return
+
+	var player: Node = network_player.instantiate()
+	player.name = str(id)
+
 	get_node(spawn_path).call_deferred("add_child", player)
-	
