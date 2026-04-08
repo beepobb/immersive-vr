e0b2bd7805e9a0929e9c949cc3368b2cece14cc9
thamhaowei <thamhaowei@gmail.com>
2026-04-08 10:23:38 +0800

    feat: hide local avatar mesh

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -47,11 +47,20 @@ func _ready():
 		if $RightController.button_pressed.is_connected(_on_right_controller_button_pressed):
 			$RightController.button_pressed.disconnect(_on_right_controller_button_pressed)
 		$RightController/in_call_control_viewport.visible = false
+	if is_multiplayer_authority():
+		hide_all_meshes($AvatarRoot/bernard/Armature/Skeleton3D)
 	# for makehuman
 	#if in_call and is_local_player:
 		#GameState.avatar = self
 		#GameState.apply_to_avatar()
-
+func hide_all_meshes(node: Node) -> void:
+	for child in node.get_children():
+		if child is MeshInstance3D:
+			child.visible = false
+		
+		# Recurse into children
+		hide_all_meshes(child)
+		
 func _on_right_controller_button_pressed(action_name: String) -> void:
 	if not _is_local_player:
 		return
